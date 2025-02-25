20b2a4be66dd850d84daf42e359b3d0c4466cb88
ezmiller-su <ezrajmiller@hotmail.com>
2025-02-25 10:54:03 -0500

    commit1

--- code/one_package.py
+++ code/one_package.py
@@ -6,3 +6,19 @@ Calculate the total package size and display that.
 
 see one_package.png for a screenshot
 '''
+
+import streamlit as st
+import packaging
+
+st.title('Process One Package')
+packagingtxt = st.text_input('Enter package data:') # 12 eggs in 1 carton / 3 cartons in 1 box
+if packagingtxt:
+    parsed = packaging.parse_packaging(packagingtxt)
+    unit = packaging.get_unit(parsed)
+    total_units = packaging.calc_total_units(parsed)
+    st.text(parsed)
+    for x in parsed:
+        item = list(x.keys())[0]
+        quantity = list(x.values())[0]
+        st.info(f"{item} ➡️ {quantity}")
+    st.success(f"Total 📦 Size: {total_units} {unit}")
--- code/process_file.py
+++ code/process_file.py
@@ -4,3 +4,28 @@ You should output the parsed package and total package size for each package in 
 
 Screenshot available as process_file.png
 '''
+
+import streamlit as st
+import json
+import packaging
+
+pkg = []
+
+uploadedfile = st.file_uploader("Upload package file:")
+file = uploadedfile.read().decode('utf-8')
+
+pkg_count = 0
+
+for line in file.split('\n'):
+    parsed = packaging.parse_packaging(line)
+    pkg.append(parsed)
+    unit = packaging.get_unit(parsed)
+    total_units = packaging.calc_total_units(parsed)
+    pkg_count += 1
+    st.info(f"{line} ➡️ Total 📦 Size: {total_units} {unit}")
+
+new_json_name = uploadedfile.name.split('.')[0]
+
+with open(f'data/{new_json_name}.json','w') as jsonfile:
+    json.dump(pkg, jsonfile, indent=4)
+    st.success(f"💾 {pkg_count} packages written to {new_json_name}.json")
